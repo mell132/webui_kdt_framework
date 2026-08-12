ed544698944f96508cc936faf333d751b59d109f
mell132 <18946517668@163.com>
2026-08-12 20:42:10 +0800

    10.显示等待进行元素关键字设定

--- core/keywords.py
+++ core/keywords.py
@@ -1,14 +1,23 @@
 import logging
 import time
 
+
 import allure
-
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class Keywords:
 
     def __init__(self,driver):
         self.driver=driver
+    def find(self,step):
+        wait= WebDriverWait(self.driver,10)
+        locator=step["by"],step["value"]
 
+        #如果索引为None，则定位单个单元，反之定位一组元素
+        if step["index"] is None:
+            return wait.until(EC.presence_of_element_located(locator))
+        else:
+            return wait.until(EC.presence_of_element_located(locator))[step["index"]]
 
     def open(self,step):
         """打开网址"""
@@ -20,19 +29,22 @@ class Keywords:
         """点击"""
         with allure.step(f'第{step["step_num"]}步：{step["step_name"]}'):
             logging.info(f'第{step["step_num"]}步：{step["step_name"]}')
-            self.driver.find_element(step["by"],step["value"]).click()
+            #self.driver.find_element(step["by"],step["value"]).click()
+            self.driver.find(step).click()
 
     def input(self,step):
         """输入文本"""
         with allure.step(f'第{step["step_num"]}步：{step["step_name"]}'):
             logging.info(f'第{step["step_num"]}步：{step["step_name"]}')
-            self.driver.find_element(step["by"],step["value"]).send_keys(step["data"])
+            self.driver.find(step).send_keys(step["data"])
+            #self.driver.find_element(step["by"],step["value"]).send_keys(step["data"])
 
     def clear(self, step):
         """清空文本"""
         with allure.step(f'第{step["step_num"]}步：{step["step_name"]}'):
             logging.info(f'第{step["step_num"]}步：{step["step_name"]}')
-            self.driver.find_element(step["by"],step["value"]).clear()
+            #self.driver.find_element(step["by"],step["value"]).clear()
+            self.driver.find(step).clear()
 
     def wait(self,step):
         """等待"""
