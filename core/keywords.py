fe6116af2db1d98074682f4d9d6b258dee968889
mell132 <18946517668@163.com>
2026-08-13 19:47:59 +0800

    15.关键字库-补充一些关键字操作

--- core/keywords.py
+++ core/keywords.py
@@ -1,9 +1,13 @@
 import logging
+import os
 import time
 
 
 import allure
 from selenium.common import TimeoutException
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -66,4 +70,140 @@ class Keywords:
         allure.attach(
             png,
             f'第{step["step_num"]}步_{now_time}.png',
-            attachment_type=allure.attachment_type.PNG,)
+            attachment_type=allure.attachment_type.PNG)
+
+    #浏览器基本操作:刷新，前进，后退，根据句柄切换到初始窗口
+    @kw_step
+    def refresh(self,step):
+        """刷新"""
+        self.driver.refresh()
+
+    @kw_step
+    def forward(self,step):
+        """前进"""
+        self.driver.forward()
+
+    @kw_step
+    def back(self, step):
+        """后退"""
+        self.driver.back()
+
+    @kw_step
+    def switch_to_window(self, step):
+        """切换窗口（根据句柄索引）"""
+        handles=self.driver.window_handles
+        self.driver.switch_to_window(handles[step["data"]])
+
+    #下拉框操作:通过文本选择，通过值选择
+    @kw_step
+    def select_by_text(self, step):
+        """通过文本选择"""
+        select=Select(self.find(step))
+        select.select_by_visible_text(step["data"])
+
+    @kw_step
+    def select_by_value(self, step):
+        """通过值选择"""
+        select = Select(self.find(step))
+        select.select_by_value(step["data"])
+
+    #弹出框操作：接受，拒绝
+    @kw_step
+    def accept_alert(self, step):
+        """接受"""
+        self.driver.switch_to_alert.accept()
+
+    @kw_step
+    def dismiss_alert(self, step):
+        """拒绝"""
+        self.driver.switch_to_alert.dismiss()
+
+    # 滚动条操作：滚动和js执行操作
+    @kw_step
+    def scroll_to(self, step):
+        """
+        滚动到某个绝对坐标位置
+        step["data"]数据要写成字典{'x':100,'y':100}形式
+        """
+        position=eval(step["data"])
+        js=f"window.scrollTo({position.dict['x']},{position.dict['y']})"
+        self.driver.execute_script(js)
+
+    @kw_step
+    def execute_js(self, step):
+        """
+        执行js代码
+        """
+        self.driver.execute_script(step["data"])
+
+    #键鼠常用操作:双击，右击，悬停，拖拽，回车
+    @kw_step
+    def double_click(self, step):
+        """
+        双击
+        """
+        element=self.find(step)
+        action=ActionChains(self.driver)
+        action.double_click(element).perform()
+
+    @kw_step
+    def right_click(self, step):
+        """
+        右击
+        """
+        element = self.find(step)
+        action = ActionChains(self.driver)
+        action.context_click(element).perform()
+
+    @kw_step
+    def hover(self, step):
+        """
+        悬停
+        """
+        element = self.find(step)
+        action = ActionChains(self.driver)
+        action.move_to_element(element).perform()
+
+    @kw_step
+    def drag_and_drop(self, step):
+        """
+        拖拽
+        step["data"]数据要写成字典{'by':'xpath','value':'xxx'}形式
+        """
+        source=self.find(step)
+        target_dict=eval(step["data"])
+        target=self.driver.find_element(target_dict["by"], target_dict["value"])
+        action = ActionChains(self.driver)
+        action.drag_and_drop(source, target).perform()
+
+    @kw_step
+    def enter(self, step):
+        """
+        回车
+        """
+        element = self.find(step)
+        element.send_keys(Keys.ENTER)
+
+    #文件上传
+    @kw_step
+    def upload(self, step):
+        """
+        上传文件
+        """
+        relative_path=step["data"]
+        absolute_path=os.path.abspath(relative_path)
+        element = self.find(step)
+        element.send_keys(absolute_path)
+
+    #frame操作：切换到某个frame，切换主文档
+
+    @kw_step
+    def switch_to_frame(self,step):
+        """根据frame元素把焦点切换到某个frame"""
+        element=self.find(step)
+        self.driver.switch_to_frame(element)
+
+    @kw_step
+    def switch_to_default_content(self, step):
+        """把焦点切换回主文档"""
+        self.driver.switch_to_default_content()
